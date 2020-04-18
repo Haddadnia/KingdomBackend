ae28e311e8cf025d37fa876168b6e7f0f30ddca5
Justin Haddadnia <haddadnia@haddadnia-macbookpro.roam.corp.google.com>
2020-04-18 17:19:39 -0400

    init commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,8 +3,22 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
+    
+
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self) //{
+            return "Hello " + name
+//        } else {
+//            return "Hello nameless!"
+//        }
+    }
+
+    router.get("hello", "justin") { req -> String in
+      return "Hello Justin!"
+    }
+    
     router.get { req in
-        return "It works!"
+        return "It works Sarah!"
     }
     
     // Basic "Hello, world!" example
@@ -17,4 +31,21 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    
+    // Justins stuff (Ray W)
+    router.post(PlayerInfo.self, at: "addPlayer") { req, data -> String in
+        playerNames.append(data.name)
+        var allNames = ""
+        for name in playerNames {
+            allNames = allNames + name + " "
+        }
+        return "Hello \(data.name)!   All players: \(allNames)"
+    }
+}
+
+var playerNames = [String]()
+
+struct PlayerInfo: Content {
+ let name: String
 }
